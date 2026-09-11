1e7bf9ebf7596c61b0670b9293068580e3509bd1
Thanasit-2540 <thanasitnoi98@gmail.com>
2026-09-11 08:12:20 +0700

    Add files via upload

--- public/dashboard.js
+++ public/dashboard.js
@@ -118,7 +118,7 @@ function openQrModal(id, name, type) {
     `;
     
     document.getElementById('qr-code-display').innerHTML = '';
-    const url = window.location.origin + '/?meter=' + id;
+    const url = window.location.origin + '/scan.html?meter=' + id;
     new QRCode(document.getElementById('qr-code-display'), {
         text: url,
         width: 200,
